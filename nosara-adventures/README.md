3f3c22652ee2f3fdd997f293bb6f40ad94aa29be
Claudia <clamorera99@gmail.com>
2025-09-28 20:38:04 -0600

    Updates

--- nosara-adventures/README.md
+++
@@ -1,1 +0,0 @@
-# nosara-adventures
--- nosara-adventures/js/util.js
+++
@@ -1,9 +0,0 @@
-// Utilidades globales livianas
-window.$  = (sel, ctx = document) => ctx.querySelector(sel);
-window.$$ = (sel, ctx = document) => Array.from(ctx.querySelectorAll(sel));
-
-window.addErr = el => el && el.classList.add("field-error");
-window.delErr = el => el && el.classList.remove("field-error");
-
-
-
